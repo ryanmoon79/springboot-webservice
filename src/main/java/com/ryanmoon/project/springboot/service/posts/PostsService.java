108765615e9decddd03ca6da79788cc2c5d1baf7
ryanmoon79 <ryanmoon79@naver.com>
2020-05-18 00:40:35 +0900

    등록기능 완료(5/18)

---
+++ src/main/java/com/ryanmoon/project/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.ryanmoon.project.springboot.service.posts;
+
+import com.ryanmoon.project.springboot.domain.posts.PostsRepository;
+import com.ryanmoon.project.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
