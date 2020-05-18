13a73c67ba48ac22fd1016d263bd1b494be643e0
ryanmoon79 <ryanmoon79@naver.com>
2020-05-18 23:58:03 +0900

    수정, 조회기능 완료

--- src/main/java/com/ryanmoon/project/springboot/service/posts/PostsService.java
+++ src/main/java/com/ryanmoon/project/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.ryanmoon.project.springboot.service.posts;
 
+import com.ryanmoon.project.springboot.domain.posts.Posts;
 import com.ryanmoon.project.springboot.domain.posts.PostsRepository;
+import com.ryanmoon.project.springboot.web.dto.PostsResponseDto;
 import com.ryanmoon.project.springboot.web.dto.PostsSaveRequestDto;
+import com.ryanmoon.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/ryanmoon/project/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.ryanmoon.project.springboot.web.dto;
+
+import com.ryanmoon.project.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
