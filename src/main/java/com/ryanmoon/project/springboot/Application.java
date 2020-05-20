491732af30531ae1d95206cb0e1d59dea885cbf4
ryanmoon79 <ryanmoon79@naver.com>
2020-05-20 16:17:37 +0900

    JPA Auditing

--- src/main/java/com/ryanmoon/project/springboot/Application.java
+++ src/main/java/com/ryanmoon/project/springboot/Application.java
@@ -2,7 +2,9 @@ package com.ryanmoon.project.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
