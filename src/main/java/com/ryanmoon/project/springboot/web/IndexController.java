1914b3c2b0970ecb734a7f5559618a2ec95575a2
ryanmoon79 <ryanmoon79@naver.com>
2020-05-21 19:56:47 +0900

    naver 로그인 반영

--- src/main/java/com/ryanmoon/project/springboot/web/IndexController.java
+++ src/main/java/com/ryanmoon/project/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ryanmoon.project.springboot.web;
 
+import com.ryanmoon.project.springboot.config.auth.LoginUser;
 import com.ryanmoon.project.springboot.config.auth.dto.SessionUser;
 import com.ryanmoon.project.springboot.service.posts.PostsService;
 import com.ryanmoon.project.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
